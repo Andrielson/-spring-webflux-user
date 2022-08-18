375ea37740554de376b3316e060c860b355ba518
Andrielson Silva <andrielson@gmail.com>
2022-08-17 22:51:14 -0300

    First commit

---
+++ src/test/java/com/github/andrielson/spring/webflux/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andrielson.spring.webflux.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
