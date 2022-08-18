69eaa02cecad5cc974e46c584c55d8c66b80efce
Andrielson Silva <andrielson@gmail.com>
2022-08-17 22:52:10 -0300

    Add transaction entity

---
+++ src/main/java/com/github/andrielson/spring/webflux/user/entity/UserTransaction.java
@@ -0,0 +1,19 @@
+package com.github.andrielson.spring.webflux.user.entity;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+public class UserTransaction {
+
+    @Id
+    private Integer id;
+    private Integer userId;
+    private Integer amount;
+    private LocalDateTime transactionDate;
+
+}
